cfb125339bffa3b905778b6328df42ff0e3bd8bb
huma17ax <genbot.think17Virt@gmail.com>
2022-07-10 14:59:06 +0900

    キー入力処理

--- Assets/Scripts/CanvasController.cs
+++ Assets/Scripts/CanvasController.cs
@@ -20,6 +20,11 @@ public class CanvasController : MonoBehaviour
 
     private GameObject[] circles;
     private GameObject keyboard;
+    private GameObject inputs;
+
+    private bool[] pre = {false, false, false, false};
+
+    private char[] hovered_chars = {' ', ' ', ' ', ' '};
 
     void Start()
     {
@@ -47,6 +52,7 @@ public class CanvasController : MonoBehaviour
         this.circles[3] = GameObject.Find("Canvas/Circle4");
         Debug.Log(this.circles[0]);
         this.keyboard = GameObject.Find("Canvas/Keyboard");
+        this.inputs = GameObject.Find("Canvas/Inputs");
     }
 
     void Update()
@@ -66,12 +72,52 @@ public class CanvasController : MonoBehaviour
             this.circles[3].GetComponent<RectTransform>().anchoredPosition = new Vector3(v[3].x*width-width/2, -(v[3].y*height-height/2), 0);
         }
 
+        CalcHoverKey();
+
         bool[] b;
         if (this.sh_touches.TryGet(out b)) {
             this.circles[0].GetComponent<RectTransform>().localScale = new Vector3(b[0] ? 2 : 1, b[0] ? 2 : 1 , 1);
             this.circles[1].GetComponent<RectTransform>().localScale = new Vector3(b[1] ? 2 : 1, b[1] ? 2 : 1 , 1);
             this.circles[2].GetComponent<RectTransform>().localScale = new Vector3(b[2] ? 2 : 1, b[2] ? 2 : 1 , 1);
             this.circles[3].GetComponent<RectTransform>().localScale = new Vector3(b[3] ? 2 : 1, b[3] ? 2 : 1 , 1);
+
+            for (int i=0; i<4; i++) {
+                if (pre[i]==false && b[i]==true) this.inputs.GetComponent<Text>().text += this.hovered_chars[i];
+                if (this.inputs.GetComponent<Text>().text.Length > 35) this.inputs.GetComponent<Text>().text.Remove(0,1);
+            }
+            pre = b;
+        }
+
+    }
+
+    private void CalcHoverKey() {
+        string top_keys = "QWERTYUIOP";
+        string mid_keys = "ASDFGHJKL";
+        string bottom_keys = "ZXCVBNM";
+
+        for (int i=0; i<4; i++) {
+            RectTransform rt = this.keyboard.GetComponent<RectTransform>();
+            Vector2 normalized_position = this.circles[i].GetComponent<RectTransform>().anchoredPosition/ (rt.sizeDelta * rt.localScale) + new Vector2(0.5f, 0.5f);
+
+            if (normalized_position.y < 1f/3f) {
+                // bottom row
+                int x = (int)Mathf.Floor(normalized_position.x*10f-1f);
+                if (x < 0 || x > 6) this.hovered_chars[i] = ' ';
+                else this.hovered_chars[i] = bottom_keys[x];
+            }
+            else if (normalized_position.y < 2f/3f) {
+                // middle row
+                int x = (int)Mathf.Floor(normalized_position.x*10f-0.5f);
+                if (x < 0 || x > 8) this.hovered_chars[i] = ' ';
+                else this.hovered_chars[i] = mid_keys[x];
+            }
+            else {
+                // top row
+                int x = (int)Mathf.Floor(normalized_position.x*10f);
+                if (x < 0 || x > 9) this.hovered_chars[i] = ' ';
+                else this.hovered_chars[i] = top_keys[x];
+            }
+
         }
     }
 
