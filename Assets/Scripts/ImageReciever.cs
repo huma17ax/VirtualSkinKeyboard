80c19e494c9021d399369d72a406f44a6cccd48c
huma17ax <genbot.think17Virt@gmail.com>
2022-07-13 02:09:25 +0900

    手を前面表示する

--- Assets/Scripts/ImageReciever.cs
+++ Assets/Scripts/ImageReciever.cs
@@ -3,11 +3,13 @@ using System;
 
 public class ImageReceiver: ThreadRunner
 {
-    private SharedData<Color32[]> sh_colors;
+    private SharedData<Color32[]> sh_background;
+    private SharedData<Color32[]> sh_foreground;
 
-    public ImageReceiver(SharedData<Color32[]> sh_colors)
+    public ImageReceiver(SharedData<Color32[]> sh_background, SharedData<Color32[]> sh_foreground)
     {
-        this.sh_colors = sh_colors;
+        this.sh_background = sh_background;
+        this.sh_foreground = sh_foreground;
     }
 
     protected override void Run()
@@ -24,7 +26,8 @@ public class ImageReceiver: ThreadRunner
                     {
                         byte[] bytes = pipe.Read(640 * 480 * 4);
                         if (bytes == null) break;
-                        this.sh_colors.Set(BytesToColors(bytes));
+                        this.sh_background.Set(BytesToColorsNotMasked(bytes));
+                        this.sh_foreground.Set(BytesToColors(bytes));
                     }
                 }
                 catch (Exception e)
@@ -50,7 +53,20 @@ public class ImageReceiver: ThreadRunner
             colors[i].b = bytes[4 * i + 2];
             colors[i].a = bytes[4 * i + 3];
         }
+        return colors;
+    }
 
+    private Color32[] BytesToColorsNotMasked(byte[] bytes)
+    {
+        Color32[] colors = new Color32[bytes.Length / 4];
+
+        for (int i = 0; i < colors.Length; i++)
+        {
+            colors[i].r = bytes[4 * i + 0];
+            colors[i].g = bytes[4 * i + 1];
+            colors[i].b = bytes[4 * i + 2];
+            colors[i].a = 255;
+        }
         return colors;
     }
 }
