443daebaba4186a4863d833163586a3d324bb137
huma17ax <genbot.think17Virt@gmail.com>
2022-09-03 00:01:07 +0900

    接触判定のログ形式を変更

--- Backend/touch_detector.py
+++ Backend/touch_detector.py
@@ -40,7 +40,7 @@ class TouchDetector(Thread):
                 touches = self.model.predict([
                     image.reshape((1,1,50,50,3)) for image in cropped_images
                 ])
-                logging([int(round(t[0][0][0])) for t in touches])
+                logging([t[0][0][0].item() for t in touches])
                 self.sh_touches.set([t[0][0][0] > 0.5 for t in touches])
         print('TOUCH DETECTOR END')
     
